fb06fd149fc2b4d8774a47fc677470f84cc8c161
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-09-01 00:39:33 -0300

    feat: change fotos and styling menu bar

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.scss']
+  styleUrls: ['./big-card.component.scss'],
 })
 export class BigCardComponent {
-
+  private pathToAssets: string = '../../../assets';
+  octocat: string = `${this.pathToAssets}/octocat.png`;
 }
--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.scss']
+  styleUrls: ['./menu-bar.component.scss'],
 })
 export class MenuBarComponent {
-
+  private pathToAssets: string = '../../../assets/';
+  linkedinLogo: string = `${this.pathToAssets}/linkedin.png`;
+  githubLogo: string = `${this.pathToAssets}/github.png`;
 }
