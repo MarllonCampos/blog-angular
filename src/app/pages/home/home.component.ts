b7c9752b3507ee19a29a514077ef4c98b7765fe4
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-09-05 00:49:27 -0300

    feat: make small card accept dynamic input

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BigCards, Cards } from 'src/app/cards';
 
 @Component({
   selector: 'app-home',
@@ -6,21 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  relatedPosts: Array<Number> = [1, 2, 3];
+  relatedPosts: Array<Cards> = [
+    {
+      date: 'July 05, 2020',
+      title: 'Atenção! Data especial a frente',
+      photoCover:
+        'https://images.pexels.com/photos/1415131/pexels-photo-1415131.jpeg?auto=compress&cs=tinysrgb&w=1200',
+    },
+    {
+      date: 'March 21, 2000',
+      title: 'Atenção! Data especial a frente',
+      photoCover:
+        'https://images.pexels.com/photos/189296/pexels-photo-189296.jpeg?auto=compress&cs=tinysrgb&w=1200',
+    },
+    {
+      date: 'January 04, 2019',
+      title: 'Atenção! Data especial a frente',
+      photoCover:
+        'https://images.pexels.com/photos/4695800/pexels-photo-4695800.jpeg?auto=compress&cs=tinysrgb&w=1200',
+    },
+  ];
   pathToAssets = '../../../assets';
 
   photoCover: string = `${this.pathToAssets}/octocat.png`;
 
-  bigCardInfo: {
-    photoCover: string;
-    cardDescription: string;
-    cardTitle: string;
-    cardDate: string;
-  } = {
+  bigCardInfo: BigCards = {
     photoCover: this.photoCover,
-    cardTitle: 'ATENÇÃO!! A vida muda hoje: Saiu o filme da pantera',
-    cardDescription:
+    title: 'ATENÇÃO!! A vida muda hoje: Saiu o filme da pantera',
+    description:
       'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel facere, architecto placeat aut pariatur dignissimos quo distinctio tempore sunt ea repellendus nobis quidem accusamus, explicabo necessitatibus dolorum quis commodi fugit.',
-    cardDate: 'March 19, 2022',
+    date: 'March 19, 2022',
   };
 }
